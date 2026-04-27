dca06a8d7583b213aa61558f20cd3d87cd88bd3c
ISAAC-Neumoton <isaacujay@gmail.com>
2026-04-27 14:47:31 +0100

    fix: correct base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ujay/',   // ← your repo name here
-  plugins: [react()],
+  base: '/Portfolio/',   // ← your repo name here
+  plugins: [react()]
 })
